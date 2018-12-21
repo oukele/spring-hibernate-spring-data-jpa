ceaacda093f57496f481127db72183adcdb01dca
oukele <32951453+oukele@users.noreply.github.com>
2018-12-21 17:03:58 +0800

    Update UserService.java

--- the_data_jpa/service/UserService.java
+++ the_data_jpa/service/UserService.java
@@ -7,10 +7,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    
+    
     @Autowired
-    private UserDao userDao;
-
+    private UserDao userDao;// userDao可以实现 JpaRepository接口中 一些已经写好的方法
+    
     public User findNameAndPassword(String name,String password){
+        //这个是 驼峰命名法 来定义规则的get 和 find 代表 sql 里的 select By 代表 select 后面的条件 Name 并且 Password   (参数1，参数2 ) 
         return userDao.findByNameAndPassword(name, password);
     }
 
